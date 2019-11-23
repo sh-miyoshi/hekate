8ae1550c83ecbf61f05e5032e8fe74cfa8e3a2ef
Shunsuke Miyoshi <swfft814@gmail.com>
2019-11-23 22:35:06 +0900

    add user api base

---
+++ pkg/useapi/v1/handler.go
@@ -0,0 +1,48 @@
+package userapi
+
+import (
+	"github.com/sh-miyoshi/jwt-server/pkg/logger"
+	"net/http"
+)
+
+// AllUserGetHandler ...
+func AllUserGetHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Not implemented yet")
+	http.Error(w, "Not Implemented yet", http.StatusInternalServerError)
+}
+
+// UserCreateHandler ...
+func UserCreateHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Not implemented yet")
+	http.Error(w, "Not Implemented yet", http.StatusInternalServerError)
+}
+
+// UserDeleteHandler ...
+func UserDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Not implemented yet")
+	http.Error(w, "Not Implemented yet", http.StatusInternalServerError)
+}
+
+// UserGetHandler ...
+func UserGetHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Not implemented yet")
+	http.Error(w, "Not Implemented yet", http.StatusInternalServerError)
+}
+
+// UserUpdateHandler ...
+func UserUpdateHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Not implemented yet")
+	http.Error(w, "Not Implemented yet", http.StatusInternalServerError)
+}
+
+// UserRoleAddHandler ...
+func UserRoleAddHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Not implemented yet")
+	http.Error(w, "Not Implemented yet", http.StatusInternalServerError)
+}
+
+// UserRoleDeleteHandler ...
+func UserRoleDeleteHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Info("Not implemented yet")
+	http.Error(w, "Not Implemented yet", http.StatusInternalServerError)
+}
---
+++ pkg/useapi/v1/types.go
@@ -0,0 +1,26 @@
+package userapi
+
+// UserCreateRequest ...
+type UserCreateRequest struct {
+	Name     string `json:"name"`
+	Password string `json:"password"`
+	Enabled  bool   `json:"enabled"`
+}
+
+// UserGetResponse ...
+type UserGetResponse struct {
+	ID           string   `json:"id"`
+	Name         string   `json:"name"`
+	Enabled      bool     `json:"enabled"`
+	PasswordHash string   `json:"passwordHash"`
+	CreatedAt    string   `json:"createdAt"`
+	Roles        []string `json:"roles"` // Array of role IDs
+}
+
+// UserPutRequest ...
+type UserPutRequest struct {
+	Name     string   `json:"name"`
+	Enabled  bool     `json:"enabled"`
+	Password string   `json:"password"`
+	Roles    []string `json:"roles"` // Array of role IDs
+}
