dc1a21f71055f8005f948a90285225911618f6ae
Shunsuke Miyoshi <swfft814@gmail.com>
2019-11-25 00:10:29 +0900

    update project struct

--- pkg/db/model/project.go
+++ pkg/db/model/project.go
@@ -4,10 +4,19 @@ import (
 	"errors"
 )
 
+// TokenConfig ...
+type TokenConfig struct {
+	AccessTokenLifeSpan  int32
+	RefreshTokenLifeSpan int32
+}
+
 // ProjectInfo ...
 type ProjectInfo struct {
-	ID   string
-	Name string
+	ID          string
+	Name        string
+	Enabled     bool
+	CreatedAt   string
+	TokenConfig *TokenConfig
 }
 
 var (
