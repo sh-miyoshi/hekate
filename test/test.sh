c918d93a31729ef5a1d328b455413edb96390f8d
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-12-02 00:06:22 +0000

    update test script

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,14 @@
 URL="http://localhost:8080/api/v1"
 PROJECT_ID="master"
 
-access_token=`curl -X POST -d '@token_request.json' $URL/project/$PROJECT_ID/token | jq -r .accessToken`
+master_token=`curl -s -X POST -d '@token_request.json' $URL/project/$PROJECT_ID/token | jq -r .accessToken`
+
+# Create New Project
+project=`curl -s -X POST -d '@project_create.json' -H "Authorization: Bearer $master_token" $URL/project | jq .`
+project_id=`echo $project | jq -r .id`
+
+# Update Project
+curl -s -X PUT -d '@project_update.json' -H "Authorization: Bearer $master_token" $URL/project/$project_id
 
 # Get All Projects
-curl -H "Authorization: Bearer $access_token" $URL/project
+curl -s -H "Authorization: Bearer $master_token" $URL/project
