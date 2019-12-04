4761b7501ae1564b15a279f42630188f6e488417
s.miyoshi <s.miyoshi1991@gmail.com>
2019-12-04 22:12:55 +0900

    add test for refresh token

--- test/test.sh
+++ test/test.sh
@@ -3,7 +3,9 @@
 URL="http://localhost:8080/api/v1"
 PROJECT_ID="master"
 
-master_token=`curl -s -X POST -d '@token_request.json' $URL/project/$PROJECT_ID/token | jq -r .accessToken`
+token_info=`curl -s -X POST -d '@token_request.json' $URL/project/$PROJECT_ID/token`
+master_token=`echo $token_info | jq -r .accessToken`
+refresh_token=`echo $token_info | jq -r .refreshToken`
 
 # Create New Project
 project=`curl -s -X POST -d '@project_create.json' -H "Authorization: Bearer $master_token" $URL/project | jq .`
@@ -13,4 +15,19 @@ project_id=`echo $project | jq -r .id`
 curl -s -X PUT -d '@project_update.json' -H "Authorization: Bearer $master_token" $URL/project/$project_id
 
 # Get All Projects
+echo "all projects:"
 curl -s -H "Authorization: Bearer $master_token" $URL/project
+
+# Token Update
+echo "before token: $master_token"
+cat << EOF > new_token_request.json
+{
+    "name": "admin",
+    "secret": "$refresh_token",
+    "authType": "refresh"
+}
+EOF
+new_token_info=`curl -s -X POST -d '@new_token_request.json' $URL/project/$PROJECT_ID/token`
+rm -f new_token_request.json
+new_token=`echo $new_token_info | jq -r .accessToken`
+echo "after token: $new_token"
