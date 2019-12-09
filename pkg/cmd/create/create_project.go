9ed9b20751b8e2f4c39766384328fd481b7ece1e
Shunsuke Miyoshi <swfft814@gmail.com>
2019-12-09 23:45:47 +0900

    add project create command args

--- pkg/cmd/create/create_project.go
+++ pkg/cmd/create/create_project.go
@@ -5,6 +5,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
+type projectInfo struct {
+	Name                 string
+	Enabled              bool
+	AccessTokenLifeSpan  int
+	RefreshTokenLifeSpan int
+}
+
+var project projectInfo
+
 var createProjectCmd = &cobra.Command{
 	Use:   "project",
 	Short: "Create New Project",
@@ -12,4 +21,12 @@ var createProjectCmd = &cobra.Command{
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println("create new project")
 	},
+}
+
+func init() {
+	createProjectCmd.Flags().StringVarP(&project.Name, "name", "n", "", "[Required] set a name of new project")
+	createProjectCmd.Flags().BoolVarP(&project.Enabled, "enable", "", true, "set project enable")
+	createProjectCmd.Flags().IntVarP(&project.AccessTokenLifeSpan, "accessExpires", "", 5*60, "access token life span [sec]")
+	createProjectCmd.Flags().IntVarP(&project.RefreshTokenLifeSpan, "refreshExpires", "", 14*24*60*60, "refresh token life span [sec]")
+	createProjectCmd.MarkFlagRequired("name")
 }
