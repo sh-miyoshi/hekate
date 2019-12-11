28a967af53ee429fa7da1d15e445383eae4e0870
Shunsuke Miyoshi <swfft814@gmail.com>
2019-12-11 21:16:08 +0900

    implement create project command

--- pkg/cmd/create/create_project.go
+++ pkg/cmd/create/create_project.go
@@ -1,14 +1,16 @@
 package create
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/sh-miyoshi/jwt-server/pkg/cmd/config"
 	"github.com/sh-miyoshi/jwt-server/pkg/logger"
 	projectapi "github.com/sh-miyoshi/jwt-server/pkg/projectapi/v1"
 	tokenapi "github.com/sh-miyoshi/jwt-server/pkg/tokenapi/v1"
 	"github.com/spf13/cobra"
-	yaml "gopkg.in/yaml.v2"
 	"io/ioutil"
+	"net/http"
 	"os"
 	"path/filepath"
 )
@@ -37,14 +39,14 @@ var createProjectCmd = &cobra.Command{
 		}
 
 		var secret tokenapi.TokenResponse
-		if err = yaml.Unmarshal(buf, &secret); err != nil {
-			logger.Error("<Program Bug> Failed to marshal secret yaml: %v", err)
+		if err = json.Unmarshal(buf, &secret); err != nil {
+			fmt.Printf("Failed to parse secret json: %v", err)
 			os.Exit(1)
 		}
 
-		serverAddr := config.Get().ServerAddr
-		logger.Debug("server address: %s", serverAddr)
+		// TODO(Validate secret)
 
+		serverAddr := config.Get().ServerAddr
 		req := projectapi.ProjectCreateRequest{
 			Name:    project.Name,
 			Enabled: project.Enabled,
@@ -53,9 +55,40 @@ var createProjectCmd = &cobra.Command{
 				RefreshTokenLifeSpan: project.RefreshTokenLifeSpan,
 			},
 		}
-		logger.Debug("project create request: %v", req)
 
-		fmt.Println("create new project")
+		url := fmt.Sprintf("%s/api/v1/project", serverAddr)
+		body, err := json.Marshal(req)
+		if err != nil {
+			logger.Error("<Program Bug> Failed to marshal JSON: %v", err)
+			os.Exit(1)
+		}
+
+		httpReq, err := http.NewRequest("POST", url, bytes.NewReader(body))
+		if err != nil {
+			logger.Error("<Program Bug> Failed to create http request: %v", err)
+			os.Exit(1)
+		}
+		httpReq.Header.Add("Content-Type", "application/json")
+		httpReq.Header.Add("Authorization", fmt.Sprintf("bearer %s", secret.AccessToken))
+		client := &http.Client{}
+		httpRes, err := client.Do(httpReq)
+		if err != nil {
+			fmt.Printf("Failed to request server: %v", err)
+			os.Exit(1)
+		}
+		defer httpRes.Body.Close()
+
+		switch httpRes.StatusCode {
+		case 200:
+			var res projectapi.ProjectGetResponse
+			if err := json.NewDecoder(httpRes.Body).Decode(&res); err != nil {
+				logger.Error("<Program Bug> Failed to parse http response: %v", err)
+				os.Exit(1)
+			}
+
+			// TODO(print)
+			fmt.Printf("result: %v", res)
+		}
 	},
 }
 
