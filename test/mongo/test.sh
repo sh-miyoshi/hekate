872fdbd56e4b21f40b21419d65bef4b0fd11e111
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-12-19 07:40:15 +0000

    add test base script

---
+++ test/mongo/test.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# Run mongodb with docker
+CONTAINER_NAME="jwt-mongo"
+STATUS=`docker ps | grep $CONTAINER_NAME`
+if [ "x$STATUS" = "x" ]; then
+  docker run --name $CONTAINER_NAME -d -p 27017:27017 \
+    -e MONGO_INITDB_ROOT_USERNAME=root \
+    -e MONGO_INITDB_ROOT_PASSWORD=example \
+    mongo
+fi
+
+# Run jwt-server with mongo driver
+cat << EOF > config.yaml
+server_port: 8080
+server_bind_address: "0.0.0.0"
+logfile: ''
+debug_mode: true
+db:
+  type: "mongo"
+  connection_string: "mongodb://root:example@localhost:27017"
+admin_name: admin
+admin_password: password
+token_issuer: jwt-server
+token_secret_key: testsecretkey
+EOF
+
+cd ../../cmd/server
+go build
+./server --config=../../test/mongo/config.yaml
+
+# Test API
