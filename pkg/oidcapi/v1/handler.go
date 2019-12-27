d31ee7527bbb21977828b09f23272a8f5f74cbac
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2019-12-27 05:14:17 +0000

    implement oidc config get api

--- pkg/oidcapi/v1/handler.go
+++ pkg/oidcapi/v1/handler.go
@@ -1,14 +1,36 @@
 package oidc
 
 import (
+	"fmt"
 	jwthttp "github.com/sh-miyoshi/jwt-server/pkg/http"
 	"net/http"
+	"strings"
 )
 
 // ConfigGetHandler method return a configuration of OpenID Connect
 func ConfigGetHandler(w http.ResponseWriter, r *http.Request) {
-	res := Config{}
-	// TODO(set value)
+	// TODO(set correct ProtoSchema default: https)
+	addr := strings.TrimSuffix(fmt.Sprintf("http://%s%s", r.Host, r.RequestURI), "/.well-known/openid-configuration")
+	res := Config{
+		Issuer:                           addr,
+		AuthorizationEndpoint:            addr + "/auth",
+		TokenEndpoint:                    addr + "/token",
+		UserinfoEndpoint:                 addr + "/userinfo",
+		JwksURI:                          addr + "/certs",
+		ScopesSupported:                  []string{"openid"},
+		ResponseTypesSupported:           []string{}, // TODO(set value)
+		SubjectTypesSupported:            []string{}, // TODO(set value)
+		IDTokenSigningAlgValuesSupported: []string{"HS256"},
+		ClaimsSupported: []string{
+			"iss",
+			"aud",
+			"sub",
+			"exp",
+			"jti",
+			"iat",
+			"nbf",
+		},
+	}
 
 	jwthttp.ResponseWrite(w, "ConfigGetHandler", &res)
 }
