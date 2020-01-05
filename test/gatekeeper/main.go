e7a9dc9aff8926da219f809cff80d8a863625770
Shunsuke Miyoshi <swfft814@gmail.com>
2020-01-05 21:55:20 +0900

    add gatekeeper test scripts

---
+++ test/gatekeeper/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"github.com/gorilla/mux"
+	"io"
+	"log"
+	"net/http"
+	"os"
+	"strconv"
+)
+
+var logger = log.New(os.Stderr, "[TESTSERVER]", log.LUTC|log.LstdFlags)
+
+func defaultHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("%s method is approved", r.Method)
+
+	// return success message
+	w.Write([]byte(r.Method + " is success"))
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("Hello service was called with")
+	logger.Printf("Host: %s", r.Host)
+	fmt.Fprint(w, "Hello")
+}
+
+func echoHandler(w http.ResponseWriter, r *http.Request) {
+	logger.Printf("Echo service was called : Content-Length [%d] bytes", r.ContentLength)
+
+	length := fmt.Sprintf("%d", r.ContentLength)
+	w.Header().Set("Content-Length", length)
+	_, err := io.Copy(w, r.Body)
+	if err != nil {
+		http.Error(w, "Internal Server Error ", http.StatusInternalServerError)
+		return
+	}
+}
+func main() {
+	var logFile string
+	var httpBindPort int // defualt port number is 10000
+	flag.StringVar(&logFile, "logfile", "", "-logfile=<log-file-name>")
+	flag.IntVar(&httpBindPort, "port", 10000, "-port=PortNumber")
+	flag.Parse()
+	HTTPBindAddr := "0.0.0.0:" + strconv.Itoa(httpBindPort)
+
+	// output log to file
+	if logFile != "" {
+		file, err := os.Create(logFile)
+		if err != nil {
+			logger.Printf("failed to create logFile %v", logFile)
+			os.Exit(1)
+		}
+		//do not call file.Close() because logger write log through file.Writer
+
+		logger.SetOutput(file)
+	}
+
+	r := mux.NewRouter()
+
+	r.HandleFunc("/", defaultHandler)
+	r.HandleFunc("/hello", helloHandler).Methods("GET")
+	r.HandleFunc("/echo", echoHandler).Methods("POST")
+
+	logger.Printf("Start Server\n")
+	if err := http.ListenAndServe(HTTPBindAddr, r); err != nil {
+		logger.Printf("%v", err)
+		os.Exit(1)
+	}
+}
---
+++ test/gatekeeper/run.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+trap 'kill $(jobs -p)' EXIT
+
+go build -o test-server
+./test-server -logfile=test-server.log &
+echo "start test backend app"
+
+cd ../../cmd/jwt-server
+go build
+./jwt-server &
+echo "start jwt-server"
+
+cd ../../test/gatekeeper
+
+# wait server up
+sleep 1
+
+URL="http://localhost:8080/api/v1"
+
+# Get Master Token
+token_info=`curl --insecure -s -X POST $URL/project/master/openid-connect/token \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "username=admin" \
+  -d "password=password" \
+  -d 'grant_type=password'`
+master_access_token=`echo $token_info | jq -r .access_token`
+
+# register client
+curl --insecure -s -X POST $URL/project/master/client \
+  -H "Authorization: Bearer $master_access_token" \
+  -H "Content-Type: application/json" \
+  -d "@client.json"
+
+# wget https://github.com/keycloak/keycloak-gatekeeper/releases/download/v2.3.0/keycloak-proxy-linux-amd64
+./keycloak-proxy-linux-amd64 --config=config.yml > gatekeeper.log 2>&1 &
+
+# echo "access without gatekeeper"
+# curl http://localhost:10000/hello
+# echo ""
+
+echo "access with gatekeeper"
+curl http://localhost:3000/hello \
+  -H "Authorization: Bearer $master_access_token"
+echo ""
+
+# wait
