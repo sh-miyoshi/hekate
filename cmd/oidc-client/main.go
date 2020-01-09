16c3f2bba76a221762f507ae9ed9e80ce8b0e6be
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-01-09 08:00:25 +0000

    [WIP]add test oidc client

---
+++ cmd/oidc-client/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"github.com/coreos/go-oidc"
+	"github.com/gorilla/handlers"
+	"github.com/gorilla/mux"
+	"golang.org/x/oauth2"
+	yaml "gopkg.in/yaml.v2"
+	"net/http"
+	"os"
+)
+
+type secretInfo struct {
+	Issuer       string `yaml:"issuer"`
+	ClientID     string `yaml:"client-id"`
+	ClientSecret string `yaml:"client-secret"`
+}
+
+var secret secretInfo
+
+func setAPI(r *mux.Router) {
+	// Main API ( require auth )
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// This is test code, so check bearer token in cookie
+		if _, err := r.Cookie("Authorization"); err != nil {
+			config, _ := getOIDCConfig()
+			url := config.AuthCodeURL("")
+			http.Redirect(w, r, url, http.StatusFound)
+			return
+		}
+
+		w.Write([]byte("login success"))
+	}).Methods("GET")
+
+	r.HandleFunc("/callback", func(w http.ResponseWriter, r *http.Request) {
+		config, provider := getOIDCConfig()
+		if err := r.ParseForm(); err != nil {
+			http.Error(w, "parse form error", http.StatusInternalServerError)
+			return
+		}
+
+		accessToken, err := config.Exchange(context.Background(), r.Form.Get("code"))
+		if err != nil {
+			http.Error(w, "Can't get access token", http.StatusInternalServerError)
+		}
+
+		rawIDToken, ok := accessToken.Extra("id_token").(string)
+		if !ok {
+			http.Error(w, "missing token", http.StatusInternalServerError)
+			return
+		}
+		oidcConfig := &oidc.Config{
+			ClientID: "testapp",
+		}
+		verifier := provider.Verifier(oidcConfig)
+		idToken, err := verifier.Verify(context.Background(), rawIDToken)
+		if err != nil {
+			http.Error(w, "id token verify error", http.StatusInternalServerError)
+			return
+		}
+		// dump all id token
+		idTokenClaims := map[string]interface{}{}
+		if err := idToken.Claims(&idTokenClaims); err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Printf("%#v\n", idTokenClaims)
+		http.SetCookie(w, &http.Cookie{
+			Name:  "Authorization",
+			Value: "Bearer " + rawIDToken,
+			Path:  "/",
+		})
+		http.Redirect(w, r, "/", http.StatusFound)
+	}).Methods("GET")
+}
+
+func initSecret(filePath string) error {
+	fp, err := os.Open(filePath)
+	if err != nil {
+		return err
+	}
+	defer fp.Close()
+
+	if err := yaml.NewDecoder(fp).Decode(&secret); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func getOIDCConfig() (*oauth2.Config, *oidc.Provider) {
+	provider, err := oidc.NewProvider(context.Background(), secret.Issuer)
+	if err != nil {
+		panic(err)
+	}
+
+	oauth2Config := &oauth2.Config{
+		ClientID:     secret.ClientID,
+		ClientSecret: secret.ClientSecret,
+		Endpoint:     provider.Endpoint(),
+		Scopes:       []string{oidc.ScopeOpenID},
+		RedirectURL:  "http://localhost:3000/callback",
+	}
+
+	return oauth2Config, provider
+}
+
+func main() {
+	// read secret file
+	if err := initSecret("secret.yaml"); err != nil {
+		fmt.Printf("Failed to initialize secret: %v", err)
+		return
+	}
+
+	// Setup API
+	r := mux.NewRouter()
+	setAPI(r)
+
+	// Run Server
+	corsObj := handlers.AllowedOrigins([]string{"*"})
+	addr := "0.0.0.0:3000"
+	fmt.Printf("start server with %s\n", addr)
+	if err := http.ListenAndServe(addr, handlers.CORS(corsObj)(r)); err != nil {
+		fmt.Printf("Failed to run server: %+v\n", err)
+		os.Exit(1)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module github.com/sh-miyoshi/jwt-server
 
 require (
 	github.com/DataDog/zstd v1.4.4 // indirect
+	github.com/coreos/go-oidc v2.1.0+incompatible
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/dvsekhvalnov/jose2go v0.0.0-20180829124132-7f401d37b68a
 	github.com/go-stack/stack v1.8.0 // indirect
@@ -12,6 +13,7 @@ require (
 	github.com/gorilla/mux v1.7.2
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.8.1
+	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
 	github.com/spf13/cobra v0.0.5
 	github.com/stretchr/testify v1.4.0 // indirect
 	github.com/tidwall/pretty v1.0.0 // indirect
@@ -19,9 +21,11 @@ require (
 	github.com/xdg/stringprep v1.0.0 // indirect
 	go.mongodb.org/mongo-driver v1.2.0
 	golang.org/x/crypto v0.0.0-20190510104115-cbcb75029529
+	golang.org/x/oauth2 v0.0.0-20200107190931-bf48bf16ab8d
 	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 	golang.org/x/sys v0.0.0-20191220142924-d4481acd189f // indirect
 	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
+	gopkg.in/square/go-jose.v2 v2.4.1 // indirect
 	gopkg.in/yaml.v2 v2.2.2
 )
 
