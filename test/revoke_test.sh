484aa53e532e61517b2c5fcb9c6e68d921b28c94
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-01-22 02:39:22 +0000

    update revoke test

--- test/revoke_test.sh
+++ test/revoke_test.sh
@@ -3,36 +3,47 @@
 URL="http://localhost:8080/api/v1"
 PROJECT_NAME="master"
 
-echo "refactoring now ..."
-exit 1
+token_info=`curl --insecure -s -X POST $URL/project/$PROJECT_NAME/openid-connect/token \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "username=admin" \
+  -d "password=password" \
+  -d "client_id=admin-cli" \
+  -d 'grant_type=password'`
+access_token=`echo $token_info | jq -r .access_token`
+refresh_token=`echo $token_info | jq -r .refresh_token`
 
-token_info=`curl -s -X POST -d '@token_request.json' $URL/project/$PROJECT_NAME/token`
-master_token=`echo $token_info | jq -r .accessToken`
-refresh_token=`echo $token_info | jq -r .refreshToken`
-
-if [ "x$master_token" = "x" -o "x$refresh_token" = "x" ]; then
-    echo "Failed to get token"
-    exit 1
+if [ "$access_token" = "null" ]; then
+  echo "Failed to get access token"
+  exit 1
 fi
+if [ "$refresh_token" = "null" ]; then
+  echo "Failed to get refresh token"
+  exit 1
+fi
+echo "successfully get token"
 
 # Token Update
-cat << EOF > new_token_request.json
-{
-    "name": "admin",
-    "secret": "$refresh_token",
-    "authType": "refresh"
-}
-EOF
-new_token_info=`curl -s -X POST -d '@new_token_request.json' $URL/project/$PROJECT_NAME/token`
-new_token=`echo $new_token_info | jq -r .accessToken`
+new_token_info=`curl --insecure -s -X POST $URL/project/$PROJECT_NAME/openid-connect/token \
+  -H "Content-Type: application/x-www-form-urlencoded" \
+  -d "refresh_token=$refresh_token" \
+  -d "client_id=admin-cli" \
+  -d 'grant_type=refresh_token'`
+
+new_access_token=`echo $new_token_info | jq -r .access_token`
+new_refresh_token=`echo $new_token_info | jq -r .refresh_token`
+
+if [ "$new_access_token" = "null" ]; then
+  echo "Failed to update access token"
+  exit 1
+fi
+if [ "$new_refresh_token" = "null" ]; then
+  echo "Failed to update refresh token"
+  exit 1
+fi
+echo "successfully get new token"
 
 # TODO(Get token by previous refresh token(Expect failed))
-failed_token=`curl -s -X POST -d '@new_token_request.json' $URL/project/$PROJECT_NAME/token | jq -r .accessToken`
-echo $failed_token
 
-rm -f new_token_request.json
-
-# TODO(revoke refresh token)
-# TODO(get access token by revoked refresh token)
+# TODO(revoke refresh token, get access token by revoked refresh token)
 
 # TODO(revoke all refresh token in a project)
