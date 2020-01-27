d5da3c2bfaa3ce209cfff24bbfd57c9f8c754ba5
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-01-27 08:27:20 +0000

    add test for role

---
+++ pkg/role/handler_test.go
@@ -0,0 +1,56 @@
+package role
+
+import (
+	"testing"
+)
+
+func TestAuthorize(t *testing.T) {
+	roles := []string{
+		"read-test1",
+		"write-test1",
+		"read-test2",
+	}
+
+	tt := []struct {
+		targetResource Resource
+		roleType Type
+		expectSuccess bool
+	}{
+		{
+			Resource{"test1"},
+			Type{"read"},
+			true,
+		},
+		{
+			Resource{"test2"},
+			Type{"read"},
+			true,
+		},
+		{
+			Resource{"test1"},
+			Type{"write"},
+			true,
+		},
+		{
+			Resource{"test2"},
+			Type{"write"},
+			false,
+		},
+		{
+			Resource{"test"},
+			Type{"read"},
+			false,
+		},
+	}
+
+	handler := &Handler{}
+
+	for _, target := range tt {
+		res := target.targetResource
+		typ := target.roleType
+		result := handler.Authorize(roles, res, typ)
+		if result != target.expectSuccess {
+			t.Errorf("Authorize role %s-%s returns wrong status. got %v, want %v", res, typ, result, target.expectSuccess)
+		}
+	}
+}
