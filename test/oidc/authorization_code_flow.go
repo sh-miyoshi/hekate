e643fc752e853d49b6af7b746a2ad1a88596970f
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-01-30 00:17:21 +0000

    update test code

--- test/oidc/authorization_code_flow.go
+++ test/oidc/authorization_code_flow.go
@@ -5,17 +5,20 @@ import (
 	"fmt"
 	oidc "github.com/coreos/go-oidc"
 	"golang.org/x/oauth2"
+	"os"
+	"net/http"
 )
 
 func main() {
 	issuer := "http://localhost:8080/api/v1/project/master"
 	clientID := "admin-cli"
 	clientSecret := ""
+	state := "mystate"
 
 	provider, err := oidc.NewProvider(context.Background(), issuer)
 	if err != nil {
 		fmt.Printf("Failed to create provider: %v\n", err)
-		return
+		os.Exit(1)
 	}
 
 	config := &oauth2.Config{
@@ -28,7 +31,18 @@ func main() {
 
 	fmt.Printf("config: %v\n", config)
 
-	url := config.AuthCodeURL("")
+	url := config.AuthCodeURL(state)
 
 	fmt.Printf("url: %v\n", url)
+
+	res, err := http.Get(url)
+	if err != nil {
+		fmt.Printf("Failed to request to auth handler: %v\n", err)
+		os.Exit(1)
+	}
+
+	// maybe return user login page
+	fmt.Printf("auth res: %v\n", res)
+
+
 }
