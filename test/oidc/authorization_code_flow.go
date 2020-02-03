2e612c3661c86b32cef8a298868f6ab2d3107117
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-02-03 23:21:42 +0000

    [WIP]add oidc authorization code flow test

--- test/oidc/authorization_code_flow.go
+++ test/oidc/authorization_code_flow.go
@@ -7,10 +7,15 @@ import (
 	"golang.org/x/oauth2"
 	"os"
 	"net/http"
+	"regexp"
+	"io/ioutil"
+	"strings"
+	neturl "net/url"
 )
 
 func main() {
-	issuer := "http://localhost:8080/api/v1/project/master"
+	serverAddr := "http://localhost:8080"
+	issuer := serverAddr + "/api/v1/project/master"
 	clientID := "admin-cli"
 	clientSecret := ""
 	state := "mystate"
@@ -40,9 +45,48 @@ func main() {
 		fmt.Printf("Failed to request to auth handler: %v\n", err)
 		os.Exit(1)
 	}
+	defer res.Body.Close()
 
 	// maybe return user login page
 	fmt.Printf("auth res: %v\n", res)
 
+	bytes, _ := ioutil.ReadAll(res.Body)
+	re := regexp.MustCompile(`/*action="[^\"]+`)
+	url = re.FindString(string(bytes))
+	url = strings.TrimPrefix(url, "action=")
+	url = strings.Trim(url, "\"")
+	url = serverAddr + url
 
+	// fmt.Printf("login user page: %s\n", string(bytes))
+
+	fmt.Printf("redirect url: %s\n", url)
+	u, _ := neturl.Parse(url)
+	code := u.Query().Get("login_verify_code")
+	fmt.Printf("code: %s\n", code)
+
+	req,_ := http.NewRequest("POST", url, nil)
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+	values := neturl.Values{
+		"username": []string{"admin"},
+		"password": []string{"password"},
+		"login_verify_code": []string{code},
+	}
+	req.URL.RawQuery = values.Encode()
+	
+	client := &http.Client{}
+	client.CheckRedirect = func(req *http.Request, via []*http.Request) error {
+		return http.ErrUseLastResponse
+	}
+
+	res, err = client.Do(req)
+	if err != nil {
+		fmt.Printf("Failed to request to user login handler: %v\n", err)
+		os.Exit(1)
+	}
+	defer res.Body.Close()
+
+	fmt.Printf("login res: %v\n", res)
+
+	// TODO(check status code: 302 -> ok)
+	// TODO(exchange code to token)
 }
