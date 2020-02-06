7e9634bb3db41ef89cee1002f09673a121df65b2
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-02-06 00:27:47 +0000

    update load test

--- test/load-test.sh
+++ test/load-test.sh
@@ -11,13 +11,37 @@ LOOPS=$2
 URL="http://localhost:8080/api/v1"
 PROJECT_NAME="master"
 
+function test() {
+  token_info=`curl --insecure -s -X POST $URL/project/master/openid-connect/token \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -d "username=admin" \
+    -d "password=password" \
+    -d "client_id=admin-cli" \
+    -d 'grant_type=password'`
+  token=`echo $token_info | jq -r .access_token`
+
+  # user create
+  result=`curl -s -X POST -H "Authorization: Bearer $token" \
+    "$URL/project/master/user" \
+    -d "{\"name\": \"user$1\", \"password\": \"password\"}"`
+  userID=`echo $result | jq -r .id`
+  # user get
+  result=`curl -s -H "Authorization: Bearer $token" "$URL/project/master/user/$userID" -o /dev/null -w '%{http_code}'`
+  if [ $result -gt 300 ]; then
+    echo "Failed to get user $1"
+    exit 1
+  fi
+  # user delete
+  result=`curl -s -X DELETE -H "Authorization: Bearer $token" "$URL/project/master/user/$userID" -o /dev/null -w '%{http_code}'`
+  if [ $result -gt 300 ]; then
+    echo "Failed to delete user $1"
+    exit 1
+  fi
+}
+
 for i in `seq $LOOPS`; do
   for j in `seq $THREADS`; do
-    curl --insecure -s -X POST $URL/project/$PROJECT_NAME/openid-connect/token -o /dev/null \
-      -H "Content-Type: application/x-www-form-urlencoded" \
-      -d "username=admin" \
-      -d "password=password" \
-      -d 'grant_type=password' &
+    test $j &
   done
   wait
   echo "Done $i/$LOOPS"
