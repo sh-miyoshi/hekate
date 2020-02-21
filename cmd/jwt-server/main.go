621b8777fee38c456a2afe7eddff0ff8ed890098
s.miyoshi <s.miyoshi1991@gmail.com>
2020-02-21 20:29:01 +0900

    fix cors

--- cmd/jwt-server/main.go
+++ cmd/jwt-server/main.go
@@ -8,9 +8,9 @@ import (
 	"time"
 
 	"github.com/google/uuid"
-	"github.com/gorilla/handlers"
 	"github.com/gorilla/mux"
 	"github.com/pkg/errors"
+	"github.com/rs/cors"
 	"github.com/sh-miyoshi/jwt-server/cmd/jwt-server/config"
 	clientapiv1 "github.com/sh-miyoshi/jwt-server/pkg/apihandler/v1/client"
 	roleapiv1 "github.com/sh-miyoshi/jwt-server/pkg/apihandler/v1/customrole"
@@ -191,17 +191,30 @@ func main() {
 	setAPI(r, cfg)
 
 	// Run Server
-	corsObj := handlers.AllowedOrigins([]string{"*"})
 	addr := fmt.Sprintf("%s:%d", cfg.BindAddr, cfg.Port)
 	logger.Info("start server with %s", addr)
 
+	corsOpts := cors.New(cors.Options{
+		AllowedOrigins: []string{"*"},
+		AllowedHeaders: []string{"*"},
+		AllowedMethods: []string{
+			http.MethodGet,
+			http.MethodPost,
+			http.MethodPut,
+			http.MethodPatch,
+			http.MethodDelete,
+			http.MethodOptions,
+			http.MethodHead,
+		},
+	})
+
 	if cfg.HTTPSConfig.Enabled {
-		if err := http.ListenAndServeTLS(addr, cfg.HTTPSConfig.CertFile, cfg.HTTPSConfig.KeyFile, handlers.CORS(corsObj)(r)); err != nil {
+		if err := http.ListenAndServeTLS(addr, cfg.HTTPSConfig.CertFile, cfg.HTTPSConfig.KeyFile, corsOpts.Handler(r)); err != nil {
 			logger.Error("Failed to run server: %+v", err)
 			os.Exit(1)
 		}
 	} else {
-		if err := http.ListenAndServe(addr, handlers.CORS(corsObj)(r)); err != nil {
+		if err := http.ListenAndServe(addr, corsOpts.Handler(r)); err != nil {
 			logger.Error("Failed to run server: %+v", err)
 			os.Exit(1)
 		}
--- go.mod
+++ go.mod
@@ -16,6 +16,7 @@ require (
 	github.com/kr/pretty v0.1.0 // indirect
 	github.com/pkg/errors v0.9.1
 	github.com/pquerna/cachecontrol v0.0.0-20180517163645-1555304b9b35 // indirect
+	github.com/rs/cors v1.7.0
 	github.com/spf13/cobra v0.0.5
 	github.com/tidwall/pretty v1.0.0 // indirect
 	github.com/xdg/scram v0.0.0-20180814205039-7eeb5667e42c // indirect
