d0edf4dd7b5611b0ff6d9a3dfd625ac3e0b466cd
s.miyoshi <s.miyoshi1991@gmail.com>
2020-02-23 19:57:56 +0900

    add auth code verify

--- cmd/portal/pages/callback.vue
+++ cmd/portal/pages/callback.vue
@@ -3,3 +3,12 @@
     <p>call back ...</p>
   </div>
 </template>
+
+<script>
+export default {
+  layout: 'index',
+  created() {
+    this.$auth.AuthCode(this.$route.query.code)
+  }
+}
+</script>
--- cmd/portal/plugins/auth.js
+++ cmd/portal/plugins/auth.js
@@ -1,3 +1,6 @@
+import querystring from 'querystring'
+import axios from 'axios'
+
 class AuthHandler {
   constructor(context) {
     this.context = context
@@ -23,12 +26,56 @@ class AuthHandler {
       redirect_uri: 'http://localhost:3000/callback' // TODO(use param)
     }
 
+    // TODO(use param: project_name)
     const url =
       process.env.SERVER_ADDR +
       '/api/v1/project/master/openid-connect/auth?' +
       this._encodeQuery(opts)
 
     window.location = url
+  }
+
+  async AuthCode(authCode) {
+    // TODO(consider state)
+    const opts = {
+      grant_type: 'authorization_code',
+      client_id: 'admin-cli', // TODO(use param)
+      code: authCode
+    }
+
+    // TODO(use param: project_name, timeout)
+    const headers = {
+      'Content-Type': 'application/x-www-form-urlencoded'
+    }
+
+    const url =
+      process.env.SERVER_ADDR + '/api/v1/project/master/openid-connect/token'
+
+    const handler = axios.create({
+      headers,
+      timeout: 10000
+    })
+
+    try {
+      const res = await handler.post(url, querystring.stringify(opts))
+
+      console.log(res)
+
+      // set token to local storage and redirect to top page
+    } catch (error) {
+      console.log(error)
+      if (error.response) {
+        if (error.response.status >= 400 && error.response.status < 500) {
+          // redirect to login page
+          this.context.redirect('/')
+          return
+        }
+      }
+      this.context.error({
+        message: 'Failed to request the server',
+        statusCode: 500
+      })
+    }
   }
 }
 
