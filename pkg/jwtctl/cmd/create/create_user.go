9f95858d04515196ebf26bd8ee09f21123e9ef21
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-02-27 00:23:56 +0000

    fix flags

--- pkg/jwtctl/cmd/create/create_user.go
+++ pkg/jwtctl/cmd/create/create_user.go
@@ -11,21 +11,15 @@ import (
 	"github.com/spf13/cobra"
 )
 
-type userInfo struct {
-	ProjectName string
-	Name        string
-	Password    string
-	SystemRoles []string
-	CustomRoles []string
-}
-
-var user userInfo
+var user userapi.UserCreateRequest
 
 var createUserCmd = &cobra.Command{
 	Use:   "user",
 	Short: "Create New User",
 	Long:  "Create new user into the project",
 	Run: func(cmd *cobra.Command, args []string) {
+		projectName, _ := cmd.Flags().GetString("project")
+
 		token, err := config.GetAccessToken()
 		if err != nil {
 			fmt.Printf("%s\n", err.Error())
@@ -33,14 +27,9 @@ var createUserCmd = &cobra.Command{
 		}
 
 		handler := apiclient.NewHandler(config.Get().ServerAddr, token)
-		req := &userapi.UserCreateRequest{
-			Name:        user.Name,
-			Password:    user.Password,
-			SystemRoles: user.SystemRoles,
-			CustomRoles: user.CustomRoles,
-		}
+		req := &user
 
-		res, err := handler.UserAdd(user.ProjectName, req)
+		res, err := handler.UserAdd(projectName, req)
 		if err != nil {
 			fmt.Printf("Failed to add new user %s to %s: %v", user.Name, project.Name, err)
 			os.Exit(1)
@@ -54,7 +43,7 @@ var createUserCmd = &cobra.Command{
 func init() {
 	createUserCmd.Flags().StringVarP(&user.Name, "name", "n", "", "[Required] name of new user")
 	createUserCmd.Flags().StringVarP(&user.Password, "password", "p", "", "[Required] password of new user")
-	createUserCmd.Flags().StringVar(&user.ProjectName, "project", "", "[Required] name of the project to which the user belongs")
+	createUserCmd.Flags().String("project", "", "[Required] name of the project to which the user belongs")
 	// TODO(system roles)
 	// TODO(custom roles)
 	createUserCmd.MarkFlagRequired("name")
