8570152086ad082a6b874d9a68b3eb1d2046fd7b
s.miyoshi <s.miyoshi1991@gmail.com>
2020-03-07 15:34:19 +0900

    update header css

--- cmd/portal/components/header.vue
+++ cmd/portal/components/header.vue
@@ -1,13 +1,15 @@
 <template>
   <header class="c-header">
     <!-- logo start -->
-    <nuxt-link to="/home" class="logo">HEKATE</nuxt-link>
+    <nuxt-link to="/home" class="c-header-nav">
+      <img src="~/assets/img/logo.png" class="logo" />
+    </nuxt-link>
     <!-- logo end -->
 
-    <div class="">
-      <ul class="">
-        <li><button class="" @click="logout">Logout</button></li>
-      </ul>
+    <div class="c-header-nav ml-auto mr-4">
+      <button class="btn btn-primary px-4 btn-block" @click="logout">
+        Logout
+      </button>
     </div>
   </header>
 </template>
@@ -21,3 +23,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.logo {
+  width: 120px;
+  padding-left: 10px;
+}
+</style>
