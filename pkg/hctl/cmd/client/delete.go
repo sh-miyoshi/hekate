8597f0c3dab8f304f94ff28618055f4f7607f48c
s.miyoshi <s.miyoshi1991@gmail.com>
2020-03-12 20:19:12 +0900

    implement client delete cmd

--- pkg/hctl/cmd/client/delete.go
+++ pkg/hctl/cmd/client/delete.go
@@ -1,6 +1,11 @@
 package client
 
 import (
+	"os"
+
+	apiclient "github.com/sh-miyoshi/hekate/pkg/apiclient/v1"
+	"github.com/sh-miyoshi/hekate/pkg/hctl/config"
+	"github.com/sh-miyoshi/hekate/pkg/hctl/print"
 	"github.com/spf13/cobra"
 )
 
@@ -9,8 +14,28 @@ var deleteClientCmd = &cobra.Command{
 	Short: "Delete Client",
 	Long:  "Delete client from the project",
 	Run: func(cmd *cobra.Command, args []string) {
+		projectName, _ := cmd.Flags().GetString("project")
+		id, _ := cmd.Flags().GetString("id")
+
+		token, err := config.GetAccessToken()
+		if err != nil {
+			print.Error("Token get failed: %v", err)
+			os.Exit(1)
+		}
+
+		handler := apiclient.NewHandler(config.Get().ServerAddr, token)
+
+		if err := handler.ClientDelete(projectName, id); err != nil {
+			print.Fatal("Failed to delete the client %s to %s: %v", id, projectName, err)
+		}
+
+		print.Print("Client %s successfully deleted", id)
 	},
 }
 
 func init() {
+	deleteClientCmd.Flags().String("project", "", "[Required] name of the project to which the client belongs")
+	deleteClientCmd.Flags().String("id", "", "id of the client")
+	deleteClientCmd.MarkFlagRequired("project")
+	deleteClientCmd.MarkFlagRequired("id")
 }
