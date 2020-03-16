ac26f230a5838090c7c0553c5c802b905432834e
s.miyoshi <s.miyoshi1991@gmail.com>
2020-03-16 22:40:40 +0900

    remove unused code

--- pkg/apihandler/v1/project/handler.go
+++ pkg/apihandler/v1/project/handler.go
@@ -206,9 +206,6 @@ func ProjectUpdateHandler(w http.ResponseWriter, r *http.Request) {
 		if errors.Cause(err) == model.ErrNoSuchProject {
 			logger.Info("No such project: %s", projectName)
 			http.Error(w, "Project Not Found", http.StatusNotFound)
-		} else if errors.Cause(err) == model.ErrProjectAlreadyExists {
-			logger.Info("Project %s already exists", projectName)
-			http.Error(w, "Project Already Exists", http.StatusBadRequest)
 		} else {
 			logger.Error("Failed to get project: %+v", err)
 			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
