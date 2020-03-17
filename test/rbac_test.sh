1fcc97094bc3a7d570b7ba380aa7e3d4654f0d11
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-03-17 08:24:40 +0000

    [WIP]update rbac test script

--- test/rbac_test.sh
+++ test/rbac_test.sh
@@ -16,9 +16,11 @@ go build
 
 ./hctl login --name admin --password password
 ./hctl project add --name rbac-test
+./hctl client add --project rbac-test --name test-client
 ./hctl user add --project rbac-test --name viewer --password password --systemRoles "read-client"
 ./hctl user add --project rbac-test --name editor --password password --systemRoles "read-client,write-client"
 ./hctl project add --name rbac-test-2
+./hctl client add --project rbac-test-2 --name test-client-2
 
 #----------------------------------
 # Test read/write role
