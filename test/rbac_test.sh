39ead228a0b8bbb08a17b7090202282bdd87166d
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-03-17 00:02:36 +0000

    add rbac test prepare

--- test/rbac_test.sh
+++ test/rbac_test.sh
@@ -1,10 +1,24 @@
 #!/bin/bash
 
+CLI_DIR="../cmd/hctl"
+
+curl localhost:8080/healthz -s -o /dev/null
+if [ $? != 0 ]; then
+  echo "Before test, please run a server"
+  exit 1
+fi
+
 #----------------------------------
 # Prepare
 #----------------------------------
+cd $CLI_DIR
+go build
 
-# TODO
+./hctl login --name admin --password password
+./hctl project add --name rbac-test
+./hctl user add --project rbac-test --name viewer --password password --systemRoles "read-client"
+./hctl user add --project rbac-test --name editor --password password --systemRoles "read-client,write-client"
+./hctl project add --name rbac-test-2
 
 #----------------------------------
 # Test read/write role
