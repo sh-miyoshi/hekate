d447b3f34afc7759f3f03b94662fd697a5c3e18c
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-03-19 07:24:27 +0000

    add rbac test

--- test/rbac_test.sh
+++ test/rbac_test.sh
@@ -8,6 +8,22 @@ if [ $? != 0 ]; then
   exit 1
 fi
 
+function test_command() {
+  ./hctl $@
+  if [ $? != 0 ]; then
+    echo "hctl $@ command expect success, but got error"
+    exit 1
+  fi
+}
+
+function test_command_failed() {
+  ./hctl $@
+  if [ $? = 0 ]; then
+    echo "hctl $@ command expect failed, but successed"
+    exit 1
+  fi
+}
+
 #----------------------------------
 # Prepare
 #----------------------------------
@@ -16,17 +32,22 @@ go build
 
 ./hctl login --name admin --password password
 ./hctl project add --name rbac-test
-./hctl client add --project rbac-test --name test-client
 ./hctl user add --project rbac-test --name viewer --password password --systemRoles "read-client"
 ./hctl user add --project rbac-test --name editor --password password --systemRoles "read-client,write-client"
 ./hctl project add --name rbac-test-2
-./hctl client add --project rbac-test-2 --name test-client-2
+./hctl client add --project rbac-test-2 --id test-client-2 --accessType public
 
 #----------------------------------
 # Test read/write role
 #----------------------------------
 
-# TODO
+./htcl login --name editor --password password
+test_command client add --project rbac-test --id test-client --accessType public
+test_command client get --project rbac-test
+
+./htcl login --name viewer --password password
+test_command_failed client add --project rbac-test --id test-client-2 --accessType public
+test_command client get --project rbac-test
 
 #----------------------------------
 # Test cluster role
