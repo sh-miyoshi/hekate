c7a1873977db39f0e89e6f92d9c2a67f92512cde
s.miyoshi <s.miyoshi@jp.fujitsu.com>
2020-03-27 08:09:31 +0000

    update docker setting

--- build/allinone/run.sh
+++ build/allinone/run.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-# Run server
+if [ "x$SERVER_ADDR" = "x" ]; then
+  echo "Please set SERVER_ADDR to os env."
+  exit 1
+fi
 
-# Register callback URL for portal
+export HEKATE_PORTAL_ADDR=$SERVER_ADDR
 
 # Run Portal
+cd /myapp/portal
+npm run start > portal.log 2>&1 &
+
+# Run server
+cd /myapp/server
+./hekate-server --config=./config.yaml
