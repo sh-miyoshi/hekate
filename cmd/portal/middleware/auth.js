7bc5917a5896d3762d88276b0216d772b8aff95b
s.miyoshi <s.miyoshi1991@gmail.com>
2020-04-07 09:30:49 +0900

    change url

--- cmd/portal/middleware/auth.js
+++ cmd/portal/middleware/auth.js
@@ -1,6 +1,6 @@
 import { AuthHandler } from '../plugins/auth.js'
 
-export default async function(context) {
+export default async function (context) {
   const expiresIn = window.localStorage.getItem('expires_in')
   if (!expiresIn) {
     context.redirect('/')
@@ -11,7 +11,7 @@ export default async function(context) {
   const res = await handler.GetToken()
   if (!res.ok) {
     if (res.statusCode >= 400 && res.statusCode < 500) {
-      context.redirect('/admin')
+      context.redirect('/')
     } else {
       context.error({
         message: res.message,
