7254a7b88033852c939e111ccb2ff0bc191bd22b
s.miyoshi <s.miyoshi1991@gmail.com>
2020-04-25 10:26:16 +0900

    add unit test for user verify

---
+++ pkg/user/user_test.go
@@ -0,0 +1,66 @@
+package user
+
+import (
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/sh-miyoshi/hekate/pkg/db"
+	"github.com/sh-miyoshi/hekate/pkg/db/model"
+	"github.com/sh-miyoshi/hekate/pkg/util"
+)
+
+func TestVerify(t *testing.T) {
+	const userName = "test-user"
+	const password = "test-password"
+
+	// Initialize test DB
+	db.InitDBManager("memory", "")
+	db.GetInst().UserAdd(&model.UserInfo{
+		ID:           uuid.New().String(),
+		ProjectName:  "master",
+		Name:         userName,
+		PasswordHash: util.CreateHash(password),
+	})
+
+	tt := []struct {
+		projectName string
+		userName    string
+		password    string
+		expectOk    bool
+	}{
+		{
+			"master",
+			userName,
+			password,
+			true,
+		},
+		{
+			"wrong-project",
+			userName,
+			password,
+			false,
+		},
+		{
+			"master",
+			"wrong-user",
+			password,
+			false,
+		},
+		{
+			"master",
+			userName,
+			"wrong-password",
+			false,
+		},
+	}
+
+	for _, tc := range tt {
+		_, err := Verify(tc.projectName, tc.userName, tc.password)
+		if tc.expectOk && err != nil {
+			t.Errorf("Verify returns wrong response. got %v, want nil", err)
+		}
+		if !tc.expectOk && err == nil {
+			t.Errorf("Verify returns wrong response. got nil, but want not nil")
+		}
+	}
+}
