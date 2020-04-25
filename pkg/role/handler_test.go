859c5aad2aa9a5f3b7cd9300365bf2aa768fcb36
s.miyoshi <s.miyoshi1991@gmail.com>
2020-04-25 19:50:53 +0900

    add unit test

--- pkg/role/handler_test.go
+++ pkg/role/handler_test.go
@@ -1,6 +1,7 @@
 package role
 
 import (
+	"fmt"
 	"testing"
 )
 
@@ -49,6 +50,46 @@ func TestAuthorize(t *testing.T) {
 		result := Authorize(roles, res, typ)
 		if result != target.expectSuccess {
 			t.Errorf("Authorize role %s-%s returns wrong status. got %v, want %v", res, typ, result, target.expectSuccess)
+		}
+	}
+}
+
+func TestParse(t *testing.T) {
+	// Initialize DB
+	InitHandler()
+
+	tt := []struct {
+		input    string
+		expectOk bool
+	}{
+		{
+			"read-cluster",
+			true,
+		},
+		{
+			"badtype-cluster",
+			false,
+		},
+		{
+			"read-badresource",
+			false,
+		},
+		{
+			"readcluster",
+			false,
+		},
+	}
+
+	for _, tc := range tt {
+		res, typ, ok := GetInst().Parse(tc.input)
+		if tc.expectOk != ok {
+			t.Errorf("Parse %s returns wrong response. got %v, want %v", tc.input, ok, tc.expectOk)
+		}
+		if ok {
+			v := fmt.Sprintf("%s-%s", typ.String(), res.String())
+			if v != tc.input {
+				t.Errorf("Parse %s returns wrong response. got %s, want %s", tc.input, v, tc.input)
+			}
 		}
 	}
 }
