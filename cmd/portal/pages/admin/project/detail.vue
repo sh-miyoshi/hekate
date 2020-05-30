256793fc3cd4ef81e84ed3dea7d1b3eac1c950ac
s.miyoshi <s.miyoshi1991@gmail.com>
2020-05-30 20:06:24 +0900

    remove extra value

--- cmd/portal/pages/admin/project/detail.vue
+++ cmd/portal/pages/admin/project/detail.vue
@@ -27,7 +27,7 @@
       </div>
 
       <div class="form-group row">
-        <label for="accessTokenLifeSpan" class="col-sm-4 control-label">
+        <label class="col-sm-4 control-label">
           User Login URL
         </label>
         <div class="col-sm-7">
@@ -89,7 +89,7 @@
       </div>
 
       <div class="form-group row">
-        <label for="refreshTokenLifeSpan" class="col-sm-4 control-label">
+        <label class="col-sm-4 control-label">
           Token Signing Algorithm
         </label>
         <div class="col-sm-3">
