a1036860e4dbbd6f33b646918c3cac6041af4b54
s.miyoshi <s.miyoshi1991@gmail.com>
2020-06-01 22:24:25 +0900

    remove user page

--- cmd/portal/middleware/auth.js
+++ cmd/portal/middleware/auth.js
@@ -1,29 +1,18 @@
 import { AuthHandler } from '../plugins/auth.js'
 
 export default async function(context) {
-  let project = ''
-  let redirect = '/admin'
-  if (context.route.path.includes('/user/project/')) {
-    const values = context.route.path.split('/')
-    if (values.length >= 4) {
-      // ignore extra path
-      project = values[3]
-      redirect = '/user/project/' + project
-    }
-  }
-
   const handler = new AuthHandler(context)
 
   const loginProject = window.localStorage.getItem('login_project')
-  if (loginProject == null || project !== loginProject) {
-    handler.Login(project)
+  if (!loginProject) {
+    handler.Login()
     return
   }
 
   const res = await handler.GetToken()
   if (!res.ok) {
     if (res.statusCode >= 400 && res.statusCode < 500) {
-      context.redirect(redirect)
+      context.redirect('/')
     } else {
       context.error({
         message: res.message,
