49d4fd347b83b633562c0eb47065fa1f7abef8f7
s.miyoshi <s.miyoshi1991@gmail.com>
2020-06-27 21:45:41 +0900

    fix bug

--- test/oidc/register_callback.sh
+++ test/oidc/register_callback.sh
@@ -8,10 +8,9 @@ token_info=`curl --insecure -s -X POST http://localhost:18443/api/v1/project/mas
   -d "client_id=portal" \
   -d 'grant_type=password'`
 master_access_token=`echo $token_info | jq -r .access_token`
-echo $master_access_token
 
 # Register callback URL
 curl --insecure -s -X PUT \
-  -d "{{\"id\":\"portal\",\"access_type\":\"public\",\"allowed_callback_urls\":[\"http://localhost:3000/callback\"]}}" \
+  -d "{\"id\":\"portal\",\"access_type\":\"public\",\"allowed_callback_urls\":[\"http://localhost:3000/callback\"]}" \
   -H "Authorization: Bearer $master_access_token" \
   http://localhost:18443/api/v1/project/master/client/portal
