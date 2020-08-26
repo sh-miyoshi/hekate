02dec3d2adf35822a077758132f19d8f422b67bc
s.miyoshi <s.miyoshi1991@gmail.com>
2020-08-26 09:14:29 +0900

    update nuxt config

--- build/allinone/nuxt.config.js
+++ build/allinone/nuxt.config.js
@@ -1,11 +1,7 @@
-import path from 'path'
 import fs from 'fs'
 
 export default {
   mode: 'spa',
-	/*
-	 ** Headers of the page
-	 */
   head: {
     title: process.env.npm_package_name || '',
     meta: [
@@ -19,88 +15,37 @@ export default {
     ],
     link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }]
   },
-	/*
-	 ** Customize the progress-bar color
-	 */
   loading: { color: '#fff' },
-	/*
-	 ** Global CSS
-	 */
   css: [
     '@/assets/css/bootstrap.min.css',
     '@/assets/css/coreui.min.css',
     '@/assets/css/style.css'
   ],
-	/*
-	 ** Plugins to load before mounting the App
-	 */
   plugins: ['~/plugins/auth.js', '~/plugins/api.js', '~/plugins/validation.js'],
-	/*
-	 ** Nuxt.js dev-modules
-	 */
   buildModules: [
-    // Doc: https://github.com/nuxt-community/eslint-module
     '@nuxtjs/eslint-module'
   ],
-	/*
-	 ** Nuxt.js modules
-	 */
   modules: [
-    // Doc: https://axios.nuxtjs.org/usage
     '@nuxtjs/axios',
-    // Doc: https://github.com/nuxt-community/dotenv-module
     '@nuxtjs/dotenv',
     '@nuxtjs/font-awesome',
     'bootstrap-vue/nuxt'
   ],
-	/*
-	 ** Axios module configuration
-	 ** See https://axios.nuxtjs.org/options
-	 */
   axios: {},
-	/*
-	 ** Build configuration
-	 */
-  build: {
-		/*
-		 ** You can extend webpack config here
-		 */
-    extend(config, ctx) {
-      // Run ESLint on save
-      if (ctx.isDev && ctx.isClient) {
-        config.module.rules.push({
-          enforce: 'pre',
-          test: /\.(js|vue)$/,
-          loader: 'eslint-loader',
-          exclude: /(node_modules)/,
-          options: {
-            fix: true
-          }
-        })
-      }
-    }
-  },
-
+  build: {},
   server: {
     host: '0.0.0.0',
     port: process.env.HEKATE_PORTAL_PORT,
     https: {
-      key: fs.readFileSync(path.resolve(__dirname, 'assets/testcerts/tls.key')),
-      cert: fs.readFileSync(path.resolve(__dirname, 'assets/testcerts/tls.crt'))
+      key: fs.readFileSync('/hekatesecret/tls.key'),
+      cert: fs.readFileSync('/hekatesecret/tls.crt')
     }
   },
-
   env: {
     HEKATE_SERVER_ADDR:
       process.env.HEKATE_SERVER_ADDR || 'http://localhost:18443',
     HEKATE_PORTAL_HOST: process.env.HEKATE_PORTAL_HOST || 'localhost',
     HEKATE_PORTAL_PORT: process.env.HEKATE_PORTAL_PORT || '3000',
-    https: true,
-    SYSTEM_ROLES: [
-      'read-cluster',
-      'write-cluster',
-      'read-project',
-      'write-project'
-    ]
+    https: true
   }
 }
