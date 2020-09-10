33dad277090bd1da197c0a4937136fe8fb4c015f
s.miyoshi <s.miyoshi1991@gmail.com>
2020-09-10 13:52:49 +0900

    fix bug

--- pkg/hctl/output/output_project.go
+++ pkg/hctl/output/output_project.go
@@ -36,7 +36,7 @@ func (f *ProjectInfoFormat) ToText() (string, error) {
 	res := fmt.Sprintf("Name:                    %s\n", f.project.Name)
 	res += fmt.Sprintf("Created Time:            %s\n", f.project.CreatedAt.String())
 	res += fmt.Sprintf("Access Token Life Span:  %d [sec]\n", f.project.TokenConfig.AccessTokenLifeSpan)
-	res += fmt.Sprintf("Refresh Token Life Span: %d [sec]", f.project.TokenConfig.RefreshTokenLifeSpan)
+	res += fmt.Sprintf("Refresh Token Life Span: %d [sec]\n", f.project.TokenConfig.RefreshTokenLifeSpan)
 	res += fmt.Sprintf("Token Signing Algorithm: %s\n", f.project.TokenConfig.SigningAlgorithm)
 	res += fmt.Sprintf("Allow Grant Types:       %v\n", f.project.AllowGrantTypes)
 	res += fmt.Sprintf("Password Policies:\n")
