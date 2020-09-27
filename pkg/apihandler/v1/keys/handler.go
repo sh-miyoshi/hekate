d38bcf3b7706b834b64a2e5e41c26767f4f2827a
s.miyoshi <s.miyoshi1991@gmail.com>
2020-09-27 14:37:44 +0900

    implement api handler

---
+++ pkg/apihandler/v1/keys/handler.go
@@ -0,0 +1,69 @@
+package keysapi
+
+import (
+	"encoding/base64"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sh-miyoshi/hekate/pkg/db"
+	"github.com/sh-miyoshi/hekate/pkg/errors"
+	jwthttp "github.com/sh-miyoshi/hekate/pkg/http"
+	"github.com/sh-miyoshi/hekate/pkg/logger"
+	"github.com/sh-miyoshi/hekate/pkg/role"
+)
+
+// KeysGetHandler ...
+//   require role: read-project
+func KeysGetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	projectName := vars["projectName"]
+
+	// Authorize API Request
+	if err := jwthttp.Authorize(r, projectName, role.ResProject, role.TypeRead); err != nil {
+		errors.PrintAsInfo(errors.Append(err, "Failed to authorize header"))
+		errors.WriteHTTPError(w, "Forbidden", err, http.StatusForbidden)
+		return
+	}
+
+	// Get Project
+	project, err := db.GetInst().ProjectGet(projectName)
+	if err != nil {
+		errors.Print(errors.Append(err, "Failed to get project"))
+		errors.WriteHTTPError(w, "Internal Server Error", err, http.StatusInternalServerError)
+		return
+	}
+
+	publicKey := base64.StdEncoding.EncodeToString(project.TokenConfig.SignPublicKey)
+
+	// Return Response
+	res := KeysGetResponse{
+		Type:      project.TokenConfig.SigningAlgorithm,
+		PublicKey: publicKey,
+	}
+
+	jwthttp.ResponseWrite(w, "KeysGetHandler", &res)
+}
+
+// KeysResetHandler ...
+//   require role: write-project
+func KeysResetHandler(w http.ResponseWriter, r *http.Request) {
+	vars := mux.Vars(r)
+	projectName := vars["projectName"]
+
+	// Authorize API Request
+	if err := jwthttp.Authorize(r, projectName, role.ResProject, role.TypeWrite); err != nil {
+		errors.PrintAsInfo(errors.Append(err, "Failed to authorize header"))
+		errors.WriteHTTPError(w, "Forbidden", err, http.StatusForbidden)
+		return
+	}
+
+	// update project secret
+	if err := db.GetInst().ProjectSecretReset(projectName); err != nil {
+		errors.Print(errors.Append(err, "Failed to reset project secret"))
+		errors.WriteHTTPError(w, "Internal Server Error", err, http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	logger.Info("KeysResetHandler method successfully finished")
+}
