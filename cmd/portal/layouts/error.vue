1e38458a5b4f50b87a6cfe71205422f1d80f4bfe
s.miyoshi <s.miyoshi1991@gmail.com>
2020-10-05 09:04:54 +0900

    add error page

---
+++ cmd/portal/layouts/error.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container">
+    <div class="text-center py-4">
+      <h1>
+        <template v-if="error.statusCode === 404">
+          404 Page Not Found
+        </template>
+        <template v-else-if="error.statusCode === 500">
+          500 Internal Server Error
+        </template>
+        <template v-else>
+          Unknown Error
+        </template>
+      </h1>
+      <hr class="col-xs-12" />
+      <div v-if="error.message !== ''" class="m-2">
+        {{ error.message }}
+      </div>
+      <div>
+        <nuxt-link to="/" class="btn btn-primary">TOP</nuxt-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      required: true
+    }
+  },
+  layout: 'index'
+}
+</script>
