69520149a8fd3b25f5b7e4495cf0d688554ba2cb
s.miyoshi <s.miyoshi1991@gmail.com>
2020-11-30 22:19:19 +0900

    fix lock test

--- test/user_lock_test.sh
+++ test/user_lock_test.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 CLI_DIR="../cmd/hctl"
+SERVER_ADDR="http://localhost:18443"
 
-curl localhost:18443/healthz -s -o /dev/null
+curl $SERVER_ADDR/healthz -s -o /dev/null
 if [ $? != 0 ]; then
   echo "Before test, please run a server"
   exit 1
@@ -24,6 +25,53 @@ function test_command_failed() {
   fi
 }
 
+function test_login() {
+  project=$1
+  name=$2
+  passwd=$3
+
+  rawToken=`curl --insecure -s -X POST $SERVER_ADDR/api/v1/project/$project/openid-connect/token \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -d "username=$name" \
+    -d "password=$passwd" \
+    -d "client_id=portal" \
+    -d 'grant_type=password'`
+
+  ok=`echo $rawToken | jq .access_token`
+  if [ $ok = "null" ]; then
+    echo "login with $@ expect success, but got error"
+    echo $rawToken
+    exit 1
+  fi
+
+  # set expires time to tomorrow
+  d=`date --rfc-3339=ns -d tomorrow`
+  d=`echo $d | sed -e "s/\s/T/g"`
+
+  token=`echo $rawToken | jq ". | { projectName: \"$project\", accessToken: .access_token, accessTokenExpiresDate: \"$d\"}"`
+  echo $token > ~/.config/hekate/secret
+}
+
+function test_login_failed() {
+  project=$1
+  name=$2
+  passwd=$3
+
+  rawToken=`curl --insecure -s -X POST $SERVER_ADDR/api/v1/project/$project/openid-connect/token \
+    -H "Content-Type: application/x-www-form-urlencoded" \
+    -d "username=$name" \
+    -d "password=$passwd" \
+    -d "client_id=portal" \
+    -d 'grant_type=password'`
+
+  ok=`echo $rawToken | jq .access_token`
+  if [ $ok != "null" ]; then
+    echo "login with $@ expect failed, but got successed"
+    echo $rawToken
+    exit 1
+  fi
+}
+
 #----------------------------------
 # Prepare test project and user
 #----------------------------------
@@ -32,39 +80,40 @@ cd $CLI_DIR
 go build
 echo "start test"
 
-./hctl login --name admin --password password
+test_login "master" "admin" "password"
 ./hctl project add --name lock-test --grantTypes password --maxLoginFailure 3 --lockDuration 5 --failureResetTime 10 --userLockEnabled
 ./hctl user add --project lock-test --name tester --password password
 
 #----------------------------------
 # Test 3 times failure
 #----------------------------------
-test_command login --project lock-test --name tester --password password
-test_command_failed login --project lock-test --name tester --password invalid_password
-test_command_failed login --project lock-test --name tester --password invalid_password
-test_command_failed login --project lock-test --name tester --password invalid_password
-test_command_failed login --project lock-test --name tester --password password
+test_login "lock-test" "tester" "password"
+test_login_failed "lock-test" "tester" "invalid_password"
+test_login_failed "lock-test" "tester" "invalid_password"
+test_login_failed "lock-test" "tester" "invalid_password"
+test_login_failed "lock-test" "tester" "password"
 
 #----------------------------------
 # Test reset failure state by timeout
 #----------------------------------
 echo "wait 11[sec] for reset failure state"
 sleep 11
-test_command_failed login --project lock-test --name tester --password invalid_password
-test_command login --project lock-test --name tester --password password
+test_login_failed "lock-test" "tester" "invalid_password"
+test_login "lock-test" "tester" "password"
 
 #----------------------------------
 # Test force reset by api
 #----------------------------------
-./hctl login --project lock-test --name tester --password invalid_password
-./hctl login --project lock-test --name tester --password invalid_password
-./hctl login --project lock-test --name tester --password invalid_password
-./hctl login --name admin --password password
+test_login "lock-test" "tester" "password"
+test_login_failed "lock-test" "tester" "invalid_password"
+test_login_failed "lock-test" "tester" "invalid_password"
+test_login_failed "lock-test" "tester" "invalid_password"
+test_login "master" "admin" "password"
 test_command user update unlock --project lock-test --name tester
-test_command login --project lock-test --name tester --password password
+test_login "lock-test" "tester" "password"
 
 #----------------------------------
 # Post-processing
 #----------------------------------
-./hctl login --name admin --password password
+test_login "master" "admin" "password"
 ./hctl project delete --name lock-test
