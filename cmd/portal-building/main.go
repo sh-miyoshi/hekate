14ccad65745161bc32650458d0d6405d145148ea
s.miyoshi <s.miyoshi1991@gmail.com>
2020-12-06 20:44:17 +0900

    add logger

--- cmd/portal-building/main.go
+++ cmd/portal-building/main.go
@@ -8,6 +8,7 @@ import (
 	"text/template"
 
 	"github.com/gorilla/mux"
+	"github.com/sh-miyoshi/hekate/pkg/logger"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
@@ -25,15 +26,25 @@ func handler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	var port int
 	var bindAddr string
+	var logfile string
 	flag.IntVar(&port, "port", 3000, "port number of server")
 	flag.StringVar(&bindAddr, "addr", "0.0.0.0", "bind address of server")
+	flag.StringVar(&logfile, "logfile", "", "file path for log, output to STDOUT if empty")
 	flag.Parse()
+
+	if err := logger.InitLogger(true, logfile); err != nil {
+		fmt.Printf("Failed to init logger: %v", err)
+		os.Exit(1)
+	}
 
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler).Methods("GET")
 
 	addr := fmt.Sprintf("%s:%d", bindAddr, port)
+	logger.Info("Run server as %s", addr)
+
 	if err := http.ListenAndServe(addr, r); err != nil {
+		logger.Error("Failed to run server: %v", err)
 		os.Exit(1)
 	}
 }
