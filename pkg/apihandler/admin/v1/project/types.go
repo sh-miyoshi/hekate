28291b6d3a47afa6f0f8f3d152f928b8b186ff33
s.miyoshi <s.miyoshi1991@gmail.com>
2021-05-02 21:42:38 +0900

    update api

--- pkg/apihandler/admin/v1/project/types.go
+++ pkg/apihandler/admin/v1/project/types.go
@@ -2,52 +2,52 @@ package projectapi
 
 // TokenConfig ...
 type TokenConfig struct {
-	AccessTokenLifeSpan  uint   `json:"accessTokenLifeSpan"`
-	RefreshTokenLifeSpan uint   `json:"refreshTokenLifeSpan"`
-	SigningAlgorithm     string `json:"signingAlgorithm"`
+	AccessTokenLifeSpan  uint   `json:"access_token_life_span"`
+	RefreshTokenLifeSpan uint   `json:"refresh_token_life_span"`
+	SigningAlgorithm     string `json:"signing_algorithm"`
 }
 
 // PasswordPolicy ...
 type PasswordPolicy struct {
 	MinimumLength       uint     `json:"length"`
-	NotUserName         bool     `json:"notUserName"`
-	BlackList           []string `json:"blackList"`
-	UseCharacter        string   `json:"useCharacter"`
-	UseDigit            bool     `json:"useDigit"`
-	UseSpecialCharacter bool     `json:"useSpecialCharacter"`
+	NotUserName         bool     `json:"not_user_name"`
+	BlackList           []string `json:"black_list"`
+	UseCharacter        string   `json:"use_character"`
+	UseDigit            bool     `json:"use_digit"`
+	UseSpecialCharacter bool     `json:"use_special_character"`
 }
 
 // UserLock ...
 type UserLock struct {
 	Enabled          bool `json:"enabled"`
-	MaxLoginFailure  uint `json:"maxLoginFailure"`
-	LockDuration     uint `json:"lockDuration"`
-	FailureResetTime uint `json:"failureResetTime"`
+	MaxLoginFailure  uint `json:"max_logi_failure"`
+	LockDuration     uint `json:"lock_duration"`
+	FailureResetTime uint `json:"failure_reset_time"`
 }
 
 // ProjectCreateRequest ...
 type ProjectCreateRequest struct {
 	Name            string         `json:"name"`
-	TokenConfig     TokenConfig    `json:"tokenConfig"`
-	PasswordPolicy  PasswordPolicy `json:"passwordPolicy"`
-	AllowGrantTypes []string       `json:"allowGrantTypes"`
-	UserLock        UserLock       `json:"userLock"`
+	TokenConfig     TokenConfig    `json:"token_config"`
+	PasswordPolicy  PasswordPolicy `json:"password_policy"`
+	AllowGrantTypes []string       `json:"allow_grant_types"`
+	UserLock        UserLock       `json:"user_lock"`
 }
 
 // ProjectGetResponse ...
 type ProjectGetResponse struct {
 	Name            string         `json:"name"`
-	CreatedAt       string         `json:"createdAt"`
-	TokenConfig     TokenConfig    `json:"tokenConfig"`
-	PasswordPolicy  PasswordPolicy `json:"passwordPolicy"`
-	AllowGrantTypes []string       `json:"allowGrantTypes"`
-	UserLock        UserLock       `json:"userLock"`
+	CreatedAt       string         `json:"created_at"`
+	TokenConfig     TokenConfig    `json:"token_config"`
+	PasswordPolicy  PasswordPolicy `json:"password_policy"`
+	AllowGrantTypes []string       `json:"allow_grant_types"`
+	UserLock        UserLock       `json:"user_lock"`
 }
 
 // ProjectPutRequest ...
 type ProjectPutRequest struct {
-	TokenConfig     TokenConfig    `json:"tokenConfig"`
-	PasswordPolicy  PasswordPolicy `json:"passwordPolicy"`
-	AllowGrantTypes []string       `json:"allowGrantTypes"`
-	UserLock        UserLock       `json:"userLock"`
+	TokenConfig     TokenConfig    `json:"token_config"`
+	PasswordPolicy  PasswordPolicy `json:"password_policy"`
+	AllowGrantTypes []string       `json:"allow_grant_types"`
+	UserLock        UserLock       `json:"user_lock"`
 }
