822a75af11eac41c416d67be00c2c2dafb775e24
s.miyoshi <s.miyoshi1991@gmail.com>
2021-05-02 11:50:26 +0900

    update password policy error

--- pkg/secret/password_policy.go
+++ pkg/secret/password_policy.go
@@ -9,17 +9,12 @@ import (
 )
 
 var (
-	// ErrPasswordPolicyFailed ...
 	ErrPasswordPolicyFailed = errors.New("Password do not much policy", "Password do not much policy")
 
-	// ErrPasswordTooShort ...
-	ErrPasswordTooShort = errors.Append(ErrPasswordPolicyFailed, "too short")
-	// ErrSameAsUserName ...
-	ErrSameAsUserName = errors.Append(ErrPasswordPolicyFailed, "same as user name")
-	// ErrBlackListed ...
-	ErrBlackListed = errors.Append(ErrPasswordPolicyFailed, "is in black list")
-	// ErrNotContainChar ...
-	ErrNotContainChar = errors.Append(ErrPasswordPolicyFailed, "do not contain required character")
+	errPasswordTooShort = errors.Append(ErrPasswordPolicyFailed, "too short")
+	errSameAsUserName   = errors.Append(ErrPasswordPolicyFailed, "same as user name")
+	errBlackListed      = errors.Append(ErrPasswordPolicyFailed, "is in black list")
+	errNotContainChar   = errors.Append(ErrPasswordPolicyFailed, "does not contain required character")
 )
 
 const (
@@ -35,38 +30,50 @@ func CheckPassword(userName, password string, policy model.PasswordPolicy) *erro
 	if policy.MinimumLength > 0 {
 		// If minimum length value is valid, check password length
 		if uint(len(password)) < policy.MinimumLength {
-			return ErrPasswordTooShort
+			err := errPasswordTooShort.Copy()
+			err.SetDescription("password must be at least %d characters", policy.MinimumLength)
+			return err
 		}
 	}
 
 	// NotUserName
 	if policy.NotUserName && userName == password {
-		return ErrSameAsUserName
+		err := errSameAsUserName.Copy()
+		err.SetDescription("password must not be the same as user name")
+		return err
 	}
 
 	// Black List
 	if slice.Contains(policy.BlackList, password) {
-		return ErrBlackListed
+		err := errBlackListed.Copy()
+		err.SetDescription("password is in black list")
+		return err
 	}
 
 	// UseCharacter
 	if policy.UseCharacter != "" {
+		err := errNotContainChar.Copy()
+
 		switch policy.UseCharacter {
 		case model.CharacterTypeLower:
 			if !strings.ContainsAny(password, lowerChars) {
-				return ErrNotContainChar
+				err.SetDescription("password does not contain lowercase letters")
+				return err
 			}
 		case model.CharacterTypeUpper:
 			if !strings.ContainsAny(password, upperChars) {
-				return ErrNotContainChar
+				err.SetDescription("password does not contain uppercase letters")
+				return err
 			}
 		case model.CharacterTypeBoth:
 			if !strings.ContainsAny(password, lowerChars) || !strings.ContainsAny(password, upperChars) {
-				return ErrNotContainChar
+				err.SetDescription("password does not contain lowercase or uppercase letters")
+				return err
 			}
 		case model.CharacterTypeEither:
 			if !strings.ContainsAny(password, lowerChars) && !strings.ContainsAny(password, upperChars) {
-				return ErrNotContainChar
+				err.SetDescription("password does not contain alphabets")
+				return err
 			}
 		}
 	}
@@ -74,14 +81,18 @@ func CheckPassword(userName, password string, policy model.PasswordPolicy) *erro
 	// UseDigit
 	if policy.UseDigit {
 		if !strings.ContainsAny(password, digits) {
-			return ErrNotContainChar
+			err := errNotContainChar.Copy()
+			err.SetDescription("password does not contain digits")
+			return err
 		}
 	}
 
 	// UseSpecialCharacter
 	if policy.UseSpecialCharacter {
 		if !strings.ContainsAny(password, specialChars) {
-			return ErrNotContainChar
+			err := errNotContainChar.Copy()
+			err.SetDescription("password does not contain special characters")
+			return err
 		}
 	}
 
--- pkg/secret/password_policy_test.go
+++ pkg/secret/password_policy_test.go
@@ -25,61 +25,61 @@ func TestCheckPassword(t *testing.T) {
 	}{
 		{expect: nil}, // if empty policy, return ok
 		{userName: "admin", password: "Admin1234!", policy: allPol, expect: nil},                    // correct password
-		{userName: "admin", password: "Admin1!", policy: allPol, expect: ErrPasswordTooShort},       // length
-		{userName: "Admin1234!", password: "Admin1234!", policy: allPol, expect: ErrSameAsUserName}, // not user name
+		{userName: "admin", password: "Admin1!", policy: allPol, expect: errPasswordTooShort},       // length
+		{userName: "Admin1234!", password: "Admin1234!", policy: allPol, expect: errSameAsUserName}, // not user name
 		{
 			userName: "admin",
 			password: "Admin1234!",
 			policy:   model.PasswordPolicy{BlackList: []string{"Admin1234!"}},
-			expect:   ErrBlackListed,
+			expect:   errBlackListed,
 		}, // black list
 		{
 			userName: "admin",
 			password: "ADMIN1234!",
 			policy:   model.PasswordPolicy{UseCharacter: model.CharacterTypeLower},
-			expect:   ErrNotContainChar,
+			expect:   errNotContainChar,
 		}, // use chars(lower)
 		{
 			userName: "admin",
 			password: "admin1234!",
 			policy:   model.PasswordPolicy{UseCharacter: model.CharacterTypeUpper},
-			expect:   ErrNotContainChar,
+			expect:   errNotContainChar,
 		}, // use chars(upper)
 		{
 			userName: "admin",
 			password: "admin1234!",
 			policy:   model.PasswordPolicy{UseCharacter: model.CharacterTypeBoth},
-			expect:   ErrNotContainChar,
+			expect:   errNotContainChar,
 		}, // use chars(both)
 		{
 			userName: "admin",
 			password: "ADMIN1234!",
 			policy:   model.PasswordPolicy{UseCharacter: model.CharacterTypeBoth},
-			expect:   ErrNotContainChar,
+			expect:   errNotContainChar,
 		}, // use chars(both)
 		{
 			userName: "admin",
 			password: "1234567!",
 			policy:   model.PasswordPolicy{UseCharacter: model.CharacterTypeEither},
-			expect:   ErrNotContainChar,
+			expect:   errNotContainChar,
 		}, // use chars(either)
 		{
 			userName: "admin",
 			password: "AdminPasswd!",
 			policy:   model.PasswordPolicy{UseDigit: true},
-			expect:   ErrNotContainChar,
+			expect:   errNotContainChar,
 		}, // use digits
 		{
 			userName: "admin",
 			password: "Admin1234",
 			policy:   model.PasswordPolicy{UseSpecialCharacter: true},
-			expect:   ErrNotContainChar,
+			expect:   errNotContainChar,
 		}, // use special chars
 	}
 
 	for _, tc := range tt {
 		err := CheckPassword(tc.userName, tc.password, tc.policy)
-		if err != tc.expect {
+		if !errors.Contains(err, tc.expect) {
 			t.Errorf("Check %v returns wrong status. got %v, want %v", tc, err, tc.expect)
 		}
 	}
